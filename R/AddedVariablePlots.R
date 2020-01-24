261aa71e60cbb498ce602a163d85db1e8598bca3
Ludde <ludviggerdin@gmail.com>
2020-01-24 18:10:24 +0100

    Add imports, change AddedVariablePlots, and add run to playground

--- R/AddedVariablePlots.R
+++ R/AddedVariablePlots.R
@@ -1,25 +1,39 @@
-#' 
+#' AddedVariablePlots
 #'
 #' Plot added variable plots. Much credit to: https://aosmith.rbind.io/2018/01/31/added-variable-plots/
+#' 
 #' @export
 AddedVariablePlots <- function(nms, data, x.vars, fit) {
-    data <- data[, c(nms, "density", "predicted", "residuals")]
-    preddat_fun <- function(data, allvars, var) {
-        s <- summarise_at(data, vars(one_of(allvars), -one_of(var)), median) 
-        cbind(select_at(data, var), s)
-    }
-    ds <- x.vars[] %>%
-        set_names() %>%
-        map( ~preddat_fun(data, mod_vars, .x))
-    preds <- ds %>%
-        map(~augment(fit, newdata = .x) ) %>%
-        map(~mutate(.x, lower = exp(.fitted - 2*.se.fit),
-                    upper = exp(.fitted + 2*.se.fit),
-                    pred = exp(.fitted)))
-    plt <- ggplot(data, aes(x = .data[[variable]], y = pred)) +
-        geom_line(size = 1) +
-        geom_ribbon(aes(ymin = lower, ymax = upper), alpha = .25) +
-        geom_rug(sides = "b") +
-        xlab(label = xlab)
-    ggsave(paste0("partial", paste0(nms, collapse = "_"), ".png"), plt)     
+    added.variable.dfs <- x.vars %>%
+        purrr::set_names() %>%
+        purrr::map(~Summarize(data, x.vars, .x))
+    dfs.w.predictions <- added.variable.dfs %>%
+        purrr::map(~broom::augment(fit, newdata = .x)) %>%
+        purrr::map(~dplyr::mutate(.x, pred = .fitted))
+    relevant.dfs <- dfs.w.predictions[nms]
+    plot.data <- dplyr::bind_rows(lapply(names(relevant.dfs), function(nm) {
+        df <- relevant.dfs[[nm]][, c(nm, "pred")]
+        colnames(df) <- c("orig", "pred")
+        df$id <- nm
+        return (df)
+    }))
+    plt <- ggplot2::ggplot(plot.data, ggplot2::aes(x = orig, y = pred)) +
+        ggplot2::geom_line() +
+        ggplot2::facet_wrap(~id)
+
+    return(plt)
+}
+#' Summarize
+#'
+#' Creates set where all other variables but var are repeated medians. Var is maintained in original state.
+#' @param data data.frame. The data to summarize from. No default.
+#' @param allvars Character vector. Names of regressors in data. No default.
+#' @param var Character vector of length 1. Variable in data to maintain/regressor to plot in the added variable plot. No default. 
+#' @export
+Summarize <- function(data, allvars, var) {
+    s <- dplyr::summarise_at(data,
+                             dplyr::vars(dplyr::one_of(allvars), -dplyr::one_of(var)),
+                             median)
+    var.w.medians <- cbind(dplyr::select_at(data, var), s)
+    return (var.w.medians)
 }
--- playground.R
+++ playground.R
@@ -1,6 +1,7 @@
 ## File for testing
 library(devtools)
 devtools::load_all()
+`%>%` <- magrittr::`%>%`
 data <- read.csv("../data/bodyfatmen.csv")
 fit <- lm(data)
 data$predicted <- predict(fit)
@@ -9,3 +10,5 @@ data$residuals <- residuals(fit)
 x.vars <- all.vars(formula(fit))[-1]
 nm.chunks <- Chunks(x.vars, 4)
 plots <- lapply(nm.chunks, PartialResiduals, data = data)
+added.variable.plots <- lapply(nm.chunks, AddedVariablePlots,
+                               data = data, x.vars = x.vars, fit = fit)
