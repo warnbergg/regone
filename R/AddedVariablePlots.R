383bcee860a8725b222ead85215095aa222bf32e
Ludde <ludviggerdin@gmail.com>
2020-01-24 22:10:49 +0100

    Add save option

--- R/AddedVariablePlots.R
+++ R/AddedVariablePlots.R
@@ -1,9 +1,14 @@
 #' AddedVariablePlots
 #'
 #' Plot added variable plots. Much credit to: https://aosmith.rbind.io/2018/01/31/added-variable-plots/
-#' 
+#' @param nms Character vector. Regressors to plot in the facet_wrap. No defualt.
+#' @param data data.frame Data Default/No default.
+#' @param regressors Character vector. The regressor labels in data. No default.
+#' @param fit lm object. Linear Model (LM) object fit to data. No default
+#' @param save.plots Logical vector of length 1. If TRUE the added variable plots are saved to disk. Defaults to TRUE.
 #' @export
-AddedVariablePlots <- function(nms, data, x.vars, fit) {
+AddedVariablePlots <- function(nms, data, x.vars,
+                               fit, save.plots = TRUE) {
     added.variable.dfs <- x.vars %>%
         purrr::set_names() %>%
         purrr::map(~Summarize(data, x.vars, .x))
@@ -20,7 +25,8 @@ AddedVariablePlots <- function(nms, data, x.vars, fit) {
     plt <- ggplot2::ggplot(plot.data, ggplot2::aes(x = orig, y = pred)) +
         ggplot2::geom_line() +
         ggplot2::facet_wrap(~id, scales = "free")
-
+    if (save.plots)
+        ggplot2::ggsave(paste0(paste0(nms, collapse="_"), "_avp.png"), plt)
     return(plt)
 }
 #' Summarize
