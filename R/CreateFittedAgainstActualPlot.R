a11682e27b14afd13a7033b22b2ac984abb1745d
Ludde <ludviggerdin@gmail.com>
2020-01-25 08:43:46 +0100

    Update function docs, add package calls

--- R/CreateFittedAgainstActualPlot.R
+++ R/CreateFittedAgainstActualPlot.R
@@ -1,18 +1,21 @@
-#' Partial residuals
+#' CreateFittedAgainstActualPlot
 #'
-#' Plot partial residuals with ggplot::facet_grid()
-#' @param nms Character vector. Regressors to plot from data. No default.
-#' @param data data.frame. Data for prediction. No default.
+#' Plot  Much credit to: https://drsimonj.svbtle.com/visualising-residuals
+#' @param nms Character vector. Regressors to be plotted against residuals. Each regressor is individually plotted against the externally studentized residuals. No default
+#' @param data data.frame object. Data as prepared in "make.project.R". Regressor values, predicted response, and residuals. No default.
+#' @param save.plot Logical vector of length 1. If TRUE then the plot is saved to disk. Defaults to TRUE 
 #' @export
-PartialResiduals <- function(nms, data) {
+CreateFittedAgainstActualPlot <- function(nms, data, save.plot = TRUE) {
     plt <- data[, c(nms, "density", "predicted", "residuals")] %>% 
-        gather(key = "iv", value = "x", -density, -predicted, -residuals) %>%
-        ggplot(aes(x = x, y = density)) + 
-        geom_segment(aes(xend = x, yend = predicted), alpha = .2) +
-        geom_point(aes(color = residuals)) +
-        scale_color_gradient2(low = "blue", mid = "white", high = "red") +
-        guides(color = FALSE) +
-        geom_point(aes(y = predicted), shape = 1) +
-        facet_grid(~ iv, scales = "free_x")
-    ggsave(paste0(paste0(nms, collapse = "_"), ".png"), plt) 
+        tidyr::gather(key = "iv", value = "x", -density, -predicted, -residuals) %>%
+        ggplot2::ggplot(ggplot2::aes(x = x, y = density)) + 
+        ggplot2::geom_segment(ggplot2::aes(xend = x, yend = predicted), alpha = .2) +
+        ggplot2::geom_point(ggplot2::aes(color = residuals)) +
+        ggplot2::scale_color_gradient2(low = "blue", mid = "white", high = "red") +
+        ggplot2::guides(color = FALSE) +
+        ggplot2::geom_point(ggplot2::aes(y = predicted), shape = 1) +
+        ggplot2::facet_grid(~ iv, scales = "free_x")
+    if (save.plot)
+        ggsave(paste0(paste0(nms, collapse = "_"), ".png"), plt)
+    return (plt)
 }
