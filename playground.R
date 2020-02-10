49f68eeef8f652c8e24d9dcfe02631058d0b30fe
Ludde <ludviggerdin@gmail.com>
2020-02-10 09:09:00 +0100

    Add boxcox analysis

--- playground.R
+++ playground.R
@@ -1,28 +1,30 @@
-## File for testing
+
 library(devtools)
 devtools::load_all()
 `%>%` <- magrittr::`%>%`
-data <- read.csv("../data/bodyfatmen.csv")
+data <- read.csv(file = "../data/bodyfatmen.csv")
 fit <- lm(formula = density ~ ., data = data)
-b <- BootstrapEstimates(data, R = 2000)
-data$predicted <- predict(fit)
-data$residuals <- residuals(fit)
-data$r.student <- rstudent(fit)
-## ------------------------ Residual Analysis -------------------------
+b <- BootstrapEstimates(data = data, R = 2000)
+data$predicted <- predict(object = fit)
+data$residuals <- MASS::studres(fit)
+data$r.student <- rstudent(model = fit)
+## Residual Analysis
 x.vars <- all.vars(formula(fit))[-1]
-nm.chunks <- Chunks(x.vars, 4)
+nm.chunks <- Chunks(vec = x.vars, n.chunks = 4)
 qq <- CreateQQPlot(data)
 ra <- lapply(nm.chunks, function(nms) {
     CreateFittedAgainstActualPlot(data = data, nms = nms)
     CreateRegressorAgainstResidualsPlot(data = data, nms = nms)
     CreateAddedVariablePlots(fit = fit, nms = nms)
 })
-far <- CreateFittedAgainstResidualsPlot(data)
-## ------------------------ Outlier detection -------------------------
+far <- CreateFittedAgainstResidualsPlot(data = data)
+## Variable transformation
+bx <- CreateBoxCoxPlot(fit = fit)
+## Outlier detection
 cd <- CreateCooksDistancePlot(fit = fit)
 di <- CreateDffitsPlot(fit = fit)
 db <- lapply(nm.chunks, CreateDfbetaPlot, fit = fit)
-## ------------------------ Multicolinearity --------------------------
-mc.list <- GenerateMulticolinearityMeasures(data, fit)
-## ------------------------ Variable selection ------------------------
+## Multicolinearity 
+mc.list <- GenerateMulticolinearityMeasures(data = data, fit = fit)
+## Variable selection
 apr <- RunAllPossibleRegression(fit = fit)
