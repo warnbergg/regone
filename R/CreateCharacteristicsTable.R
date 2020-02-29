fa14945e68c28ae086074a63961b686fa3c19ee5
Ludde <ludviggerdin@gmail.com>
2020-02-29 12:15:17 +0100

    .

---
+++ R/CreateCharacteristicsTable.R
@@ -0,0 +1,14 @@
+#' CreateCharacteristicsTable
+#'
+#' Wrapper for tableone::CreateTableOne() to produce table showing sample characteristics
+#' @param data data.frame. Sample before predicted and residuals are added. No default
+#' @param dir Character vector of length 1. Directory in which to save the table. Defaults to "./" 
+#' @param ... Additional arguments for tableone::CreateTableOne.
+#' @export
+CreateCharacteristicsTable<- function(data, dir = "./", ...) {
+    tableone::CreateTableOne(data = data, ...) %>%
+        print(nonnormal = names(data), printToggle = FALSE) %>%
+        kableExtra::kable(format = "latex", booktabs = TRUE,
+                          caption = "Sample characteristics.") %>%
+            write(paste0(dir, "tblone.tex"))
+}
