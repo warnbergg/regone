b2a227b6c8646bddc4c3c366738887173cd341cd
Ludde <ludviggerdin@gmail.com>
2020-03-05 08:50:51 +0100

    Update manuscript, add to run analysis with summary variable

--- R/RunProject.R
+++ R/RunProject.R
@@ -1,10 +1,9 @@
 #' RunProject
 #'
 #' Run full code for project.
-#' @param run.apr Logical vector of length 1. If TRUE all possible regression is run. Note that this is quite time and resource consuming. Defaults to FALSE. 
 #' @param verbose Logical vector of length 1. If TRUE messages are printed for each stage of the project, e.g. when running residual analysis. Defaults to TRUE.
 #' @export
-RunProject <- function(run.apr = FALSE, verbose = TRUE) {
+RunProject <- function(verbose = TRUE) {
     data <- read.csv("../data/bodyfatmen.csv")
     ## Run with all variables
     if (verbose)
@@ -15,22 +14,37 @@ Running full analysis cycle with all variables and observations...
     RunFullAnalysis(
         data = data,
         dir = "./main/",
-        run.apr = run.apr,
+        verbose = verbose 
+    )
+    if (verbose)
+        message("
+Running full analysis cycle with summary variable of weight, abdomen, thigh, and hip ...
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+")
+    RunFullAnalysis(
+        data = data %>%
+            dplyr::mutate(combo = (hip*thigh*abdomen)/weight) %>%
+            dplyr::select(-c(thigh, abdomen, weight, hip)),
+        dir = "./combo/",
         verbose = verbose
     )
-    main.results <- readRDS("./main/results.rds")
-    inf.points <- main.results$influence.points
+    sum.results <- readRDS("./combo/results.rds")
+    inf.points <- sum.results$influence.points
     ## Run without influence points
     if (verbose)
         message("
-Running full analysis cycle with without influential observations...
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Running full analysis cycle with summary variable and without influential points ...
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ")
+    d <- data %>%
+            dplyr::mutate(combo = (hip*thigh*abdomen)/weight) %>%
+            dplyr::select(-c(thigh, abdomen, weight, hip))
+    d <- d[-unique(inf.points), ]
     RunFullAnalysis(
-        data = data[-inf.points, ],
+        data = d,
         dir = "./woinfluence/",
-        run.apr = run.apr,
         verbose = verbose
     )
+    CreatePerformanceTable()
     message("\nProject finished.")
 }
